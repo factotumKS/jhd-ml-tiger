77a848c3e060e6bc0bdcc7699f98821869940f6d
factotumKS <1312005925@qq.com>
2019-02-25 16:42:05 +0800

    mostly done, need dealing with int and float

--- keshe/test.c
+++ keshe/test.c
@@ -1,6 +1,7 @@
-int a = 1; 
-int b = 2;
-c = a + b;
-void main() {
-    return;
+#include<stdio.h>
+int main() {
+    int a, b = -10/3;
+    int c = -20*2+3, d = -21%10;
+    int e = 30, f;
+    printf("a:%d b:%d c:%d d:%d e:%d f:%d\n", a, b, c, d, e, f);
 }
